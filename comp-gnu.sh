8c6828c1cf566d9b367b6bbb9c3d60a93e835c2b
root <root@wjzi.local>
2023-04-10 07:41:38 -0400

    Stuff

--- comp-gnu.sh
+++ comp-gnu.sh
@@ -1,51 +1,30 @@
 #!/bin/sh
-PROL="stall"
+set -eu
 cp stall.s stall-comp.s
-LEC=${?}
-if [ ${LEC} -ne 0 ];then
-    printf "cp failed with code ${LEC}\n"
-    exit ${LEC}
-fi
-printf "asm_copy:       succeeded\n"
-java Salth n staltstd < ${PROL}.slth >> ${PROL}-comp.s
-LEC=${?}
-if [ ${LEC} -ne 0 ];then
-    printf "java failed with code ${LEC}\n"
-    exit ${LEC}
-fi
-printf "salth_compile:  succeeded\n"
-printf ".if staltstd_str_commandline_addr\n  .err # The command line address is offset from the start of the shell's static text segment\n.endif\n.set dist_end141, . - _start\n.space 1474560 - dist_end141\n" >> ${PROL}-comp.s
-LEC=${?}
-if [ ${LEC} -ne 0 ];then
-    printf "printf failed with code ${LEC}\n"
-    exit ${LEC}
-fi
-printf "src_141MBpad:   succeeded\n"
-as -o ${PROL}.o ${PROL}-comp.s
-LEC=${?}
-if [ ${LEC} -ne 0 ];then
-    printf "as failed with code ${LEC}\n"
-    exit ${LEC}
-fi
-printf "assembly:       succeeded\n"
-ld -T "./newf" -o ${PROL}.elf ${PROL}.o
-LEC=${?}
-if [ ${LEC} -ne 0 ];then
-    printf "ld failed with code ${LEC}\n"
-    exit ${LEC}
-fi
-printf "linking:        succeeded\n"
-objcopy -O binary ${PROL}.elf ${PROL}.bin
-LEC=${?}
-if [ ${LEC} -ne 0 ];then
-    printf "objcopy failed with code ${LEC}\n"
-    exit 255
-fi
-qemu-system-i386 -drive file=${PROL}.bin,index=0,if=floppy,format=raw
-LEC=${?}
-if [ ${LEC} -ne 0 ];then
-    printf "remove failed with code ${LEC}\n"
-    exit 255
-fi
-printf "execution:      succeeded\n"
-exit 0
+java Salth n staltstd < stall.slth >> stall-comp.s
+printf ".if staltstd_str_commandline_addr\n  .err # The command line address is offset from the start of the shell's static text segment\n.endif\n" >> stall-comp.s
+cat sys16.dhulb | dhulbpp - - | dhulbc 16 -tNT >> stall-comp.s
+cat ${DHULB_PATH}/src/DLib/pc/io.s >> stall-comp.s
+printf ".set dist_end6144, . - _start\n.space 6144 - dist_end6144\n" >> stall-comp.s
+as -m16 -o stall.o stall-comp.s
+strip stall.o
+FILEOFF=$(otool -l stall.o | grep "fileoff" | grep -oE '[^ ]+$')
+dd if=stall.o of=stall-inter.bin bs=${FILEOFF} skip=1
+FILEOFF=$(otool -l stall.o | grep " size" | grep -oE '[^ ]+$')
+FILEOFF=$(printf "%d" ${FILEOFF})
+dd if=stall-inter.bin of=stall.bin bs=${FILEOFF} count=1
+cp kernel.s kernel-comp.s
+as -m32 -o kernel.o kernel-comp.s
+strip kernel.o
+FILEOFF=$(otool -l kernel.o | grep "fileoff" | grep -oE '[^ ]+$')
+dd if=kernel.o of=stall-inter.bin bs=${FILEOFF} skip=1
+FILEOFF=$(otool -l kernel.o | grep " size" | grep -oE '[^ ]+$')
+FILEOFF=$(printf "%d" ${FILEOFF})
+dd if=stall-inter.bin of=kernel.bin bs=${FILEOFF} count=1
+cat kernel.bin >> stall.bin
+dd if=/dev/zero of=stall.bin bs=16 count=1 seek=511
+clang -c -fno-asynchronous-unwind-tables -target i386-pc-linux-elf -Wall -o kern-ul.elf kern.c
+ld.lld kern-ul.elf -o kern.elf
+cat kern.elf >> stall.bin
+dd if=/dev/zero of=stall.bin bs=512 count=1 seek=2879
+qemu-system-i386 -D log_qemu.txt -drive file=stall.bin,format=raw,index=0,media=disk
