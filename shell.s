fb8d55df2c581395e75f5ee4a71829dc82714ba7
radicise <93229379+radicise@users.noreply.github.com>
2023-05-08 20:42:01 -0400

    more rigorous implementation of `exit', more

--- shell.s
+++ shell.s
@@ -1,9 +1,12 @@
 shell:/*dhulbDoc-v300:function;s16 shell() call16;*/
 pushw %bp
 movw %sp,%bp
-subw $510,%sp
-pushw $0x2f
 movw %sp,%ax
+subw $1022,%sp
+pushw $0x2f
+subw $512,%ax
+pushw %ax
+subw $512,%ax
 pushw %ax
 callw shelld
 movw %bp,%sp
