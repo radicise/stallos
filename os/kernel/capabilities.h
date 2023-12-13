2d4802efea8cc10c082e6bcefee5dddfcff6a354
root <93229379+radicise@users.noreply.github.com>
2023-12-13 09:29:52 -0500

    time(2) and stime(2), stime(2) had not yet been tested

---
+++ os/kernel/capabilities.h
@@ -0,0 +1,8 @@
+#ifndef __CAPABILITIES_H__
+#define __CAPABILITIES_H__ 1
+/*
+ * Capability numerical definitions as in Linux
+ * Source: Linux '/include/uapi/linux/capability.h'
+ */
+#define CAP_SYS_TIME 25
+#endif
