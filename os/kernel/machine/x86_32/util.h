62e33f47d04b8e8f40acdcab1015b2d53bf00d71
root <93229379+radicise@users.noreply.github.com>
2024-04-11 20:56:33 -0400

    Reorganisation and compatibility with GCC on MS Windows

---
+++ os/kernel/machine/x86_32/util.h
@@ -0,0 +1,5 @@
+#ifndef __MACHINE_X86_32_UTIL_H__
+#define __MACHINE_X86_32_UTIL_H__ 1
+typedef volatile unsigned char SimpleMutex;// Not reentrant
+typedef volatile unsigned long AtomicULong;
+#endif
